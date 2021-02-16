fa0d7390bca70141367677e0d78ff4bd0320f252
ruthgg <g.g.ruth020695@gmail.com>
2021-02-16 19:48:34 +0100

    Imputation repeated for inversions with DEL genotype.
    Only General population w/50 inds and same population w/ 500 inds.
    The imputeParse.R file was updated according to the current
    workflow: no Uniformity or Quality tag, no judgements, only putting
    together information. The genotyping report will later take charge
    of filtering processes.

--- code/rscript/imputeParse.R
+++ code/rscript/imputeParse.R
@@ -18,10 +18,12 @@ if (length(args)<4) {
 
 # Example
 #  
-# args[1]<-"analysis/2020-12-13_06_imputation_merged" # Directory with imputation results in .vcf
+# args[1]<-"analysis/2021-02-16_06_imputation_merged" # Directory with imputation results in .vcf
 # args[2]<-"data/use/avery_individuals/samples_population.txt"  # Test individuals population
 # args[3]<-"data/use/inversions_info/Inversions_imputability.txt" # Inversions imputability
 # args[4]<-"tmp/test" # output directory
+
+
 
 # LOAD PACKAGES
 # =========================================================================== #
@@ -122,112 +124,116 @@ lapply(packages,FUN = function(x) {
 # Results of imputation are classified according to their reliability
 # =========================================================================== #
   
-# IMPUTATION SUMMARIZATION - Uniformity: are all results equal?
+# IMPUTATION SUMMARIZATION - Uniformity: are all results equal? 
+# DEPRECATED 
 # --------------------------------------------------------------------------- #
   
-  all_3c$Uniformity<-""
-  
-  # Known populations
-  all_3c[which(all_3c$Genotype_100 != all_3c$Genotype_250 | all_3c$Genotype_250!= all_3c$Genotype_500 | all_3c$Genotype_250!= all_3c$Genotype_100),"Uniformity"]<-"bad"
-  all_3c[which(all_3c$Genotype_100 == all_3c$Genotype_con & all_3c$Uniformity == "" ), "Uniformity"]<-"good"
-  all_3c[all_3c$Uniformity == "", "Uniformity"]<-"alert"
-  # Unknown populations
-  all_3c[(all_3c$Genotype_100 != all_3c$Genotype_250 | all_3c$Genotype_250!= all_3c$Genotype_500 | all_3c$Genotype_250!= all_3c$Genotype_100) & all_3c$Population == "ALL","Uniformity"]<-"bad"
-  all_3c[ all_3c$Uniformity != "bad" & all_3c$Population == "ALL", "Uniformity"]<-"good"
+  # all_3c$Uniformity<-""
+  # 
+  # # Known populations
+  # all_3c[which(all_3c$Genotype_100 != all_3c$Genotype_250 | all_3c$Genotype_250!= all_3c$Genotype_500 | all_3c$Genotype_250!= all_3c$Genotype_100),"Uniformity"]<-"bad"
+  # all_3c[which(all_3c$Genotype_100 == all_3c$Genotype_con & all_3c$Uniformity == "" ), "Uniformity"]<-"good"
+  # all_3c[all_3c$Uniformity == "", "Uniformity"]<-"alert"
+  # # Unknown populations
+  # all_3c[(all_3c$Genotype_100 != all_3c$Genotype_250 | all_3c$Genotype_250!= all_3c$Genotype_500 | all_3c$Genotype_250!= all_3c$Genotype_100) & all_3c$Population == "ALL","Uniformity"]<-"bad"
+  # all_3c[ all_3c$Uniformity != "bad" & all_3c$Population == "ALL", "Uniformity"]<-"good"
 
 # IMPUTATION SUMMARIZATION - Quality: do all results have a probability > 80%?
+# DEPRECATED
 # --------------------------------------------------------------------------- #  
   
-  all_3c$Quality<-""
-  
-  #Known populations
-  all_3c[which(all_3c$Probability_100 > 0.8 & all_3c$Probability_250 > 0.8 & all_3c$Probability_500 > 0.8 & all_3c$Probability_con > 0.8 ),"Quality"]<-"good"
-  all_3c[which(all_3c$Probability_100 < 0.8 & all_3c$Probability_250 < 0.8 & all_3c$Probability_500 < 0.8 & all_3c$Probability_con < 0.8 ),"Quality"]<-"bad"
-  all_3c[ all_3c$Quality == "","Quality"]<-"alert"
-  #Unknown populations
-  all_3c[(all_3c$Probability_100 > 0.8 & all_3c$Probability_250 > 0.8 & all_3c$Probability_500 > 0.8 ) & all_3c$Population == "ALL","Quality"]<-"good"
-  all_3c[(all_3c$Probability_100 < 0.8 & all_3c$Probability_250 < 0.8 & all_3c$Probability_500 < 0.8 )& all_3c$Population == "ALL","Quality"]<-"bad"
-  all_3c[ !(all_3c$Quality %in% c("good", "bad")) & all_3c$Population == "ALL","Quality"]<-"alert"
+  # all_3c$Quality<-""
+  # 
+  # #Known populations
+  # all_3c[which(all_3c$Probability_100 > 0.8 & all_3c$Probability_250 > 0.8 & all_3c$Probability_500 > 0.8 & all_3c$Probability_con > 0.8 ),"Quality"]<-"good"
+  # all_3c[which(all_3c$Probability_100 < 0.8 & all_3c$Probability_250 < 0.8 & all_3c$Probability_500 < 0.8 & all_3c$Probability_con < 0.8 ),"Quality"]<-"bad"
+  # all_3c[ all_3c$Quality == "","Quality"]<-"alert"
+  # #Unknown populations
+  # all_3c[(all_3c$Probability_100 > 0.8 & all_3c$Probability_250 > 0.8 & all_3c$Probability_500 > 0.8 ) & all_3c$Population == "ALL","Quality"]<-"good"
+  # all_3c[(all_3c$Probability_100 < 0.8 & all_3c$Probability_250 < 0.8 & all_3c$Probability_500 < 0.8 )& all_3c$Population == "ALL","Quality"]<-"bad"
+  # all_3c[ !(all_3c$Quality %in% c("good", "bad")) & all_3c$Population == "ALL","Quality"]<-"alert"
 
 # OUTPUT SPREADSHEET
 # Results of imputation are included into a spreadsheet with two sheets
 # =========================================================================== #
   
 # OUTPUT SPREADSHEET - Sheet 1: Imputation Results
+# DEPRECATED
 # --------------------------------------------------------------------------- #   
 
-  sheet1<-all_3c[order(all_3c$GLB, all_3c$Individual),c("Inversion","Individual","Population","Genotype_con","Probability_con","Genotype_100","Probability_100","Genotype_250","Probability_250","Genotype_500","Probability_500","GLB","AFR","EUR","Uniformity","Quality")]  
-  write.csv(sheet1, paste0(args[4],"/imputationResults.csv"), row.names = FALSE)
+  # sheet1<-all_3c[order(all_3c$GLB, all_3c$Individual),c("Inversion","Individual","Population","Genotype_con","Probability_con","Genotype_100","Probability_100","Genotype_250","Probability_250","Genotype_500","Probability_500","GLB","AFR","EUR","Uniformity","Quality")]  
+  # write.csv(sheet1, paste0(args[4],"/imputationResults.csv"), row.names = FALSE)
 
 # OUTPUT SPREADSHEET - Sheet 2: Summary by inversion
+# DEPRECATED
 # --------------------------------------------------------------------------- #   
-
-  # Store as function to avoid intermediate objects
-  summarize<-function(all_3c){
-  
-    # List populations
-    my.pops<-c(unique(all_3c$Population))
-  
-    # Start list
-    res_list<-list()
-    
-    for (pop in my.pops) {
-      
-      cols<-10
-      colist<-c("Population", "Imputability","Good_Geno" , "P_pop", "P_100",  "P_250","P_500", "Summary", "Heteros")
-      
-      invs<-unique(all_3c$Inversion)
-      
-      tmp<-data.frame(matrix(vector(), nrow = length(invs), ncol=cols, dimnames = list(c(1:length(invs)), c("Inversion",colist))), stringsAsFactors = FALSE )
-      tmp$Inversion<-invs
-      
-      # For each inversion and population, make a summary
-      for (inv in tmp$Inversion) {
-        
-        tmp_2<-all_3c[all_3c$Inversion == inv & all_3c$Population == pop,]
-        
-        if(pop %in% "ALL"){
-          imput<-as.character(tmp_2[1,"GLB"])
-          popmean<-NA
-        }else if(pop %in% c("EUR", "AFR")){
-          imput<-as.character(tmp_2[1,pop])
-          popmean<- mean(as.numeric(tmp_2$Probability_con))
-        }else{
-          imput<-NA
-          popmean<- mean(as.numeric(tmp_2$Probability_con))
-        }
-        
-        # Content of the summary
-        content<-c(
-          pop,
-          imput,
-          table(tmp_2$Uniformity)["good"]/sum(table(tmp_2$Uniformity)),
-          popmean,
-          mean(as.numeric(tmp_2$Probability_100)),
-          mean(as.numeric(tmp_2$Probability_250)),
-          mean(as.numeric(tmp_2$Probability_500)) ,
-          sum(table(tmp_2[tmp_2$Uniformity == "good","Genotype_100"]) > 0),
-          table(tmp_2[tmp_2$Uniformity == "good","Genotype_100"])["HET"] 
-        )
-        
-        tmp[tmp$Inversion == inv, colist] <- content
-        
-      }
-      
-      res_list[[pop]]<-tmp
-      # rm(tmp, tmp_2, content)
-    }
-    
-    inversion_summary<-do.call(rbind, res_list)
-    inversion_summary<-inversion_summary[order(inversion_summary$Inversion),]
-    
-    inversion_summary[is.na(inversion_summary$Heteros),"Heteros"]<-0
-    
-    return(inversion_summary)
-  }
-  
-  sheet2<-summarize(all_3c)
-  write.csv(sheet2,   paste0(args[4],"/summaryByInversion.csv"), row.names = FALSE) 
+# 
+#   # Store as function to avoid intermediate objects
+#   summarize<-function(all_3c){
+#   
+#     # List populations
+#     my.pops<-c(unique(all_3c$Population))
+#   
+#     # Start list
+#     res_list<-list()
+#     
+#     for (pop in my.pops) {
+#       
+#       cols<-10
+#       colist<-c("Population", "Imputability","Good_Geno" , "P_pop", "P_100",  "P_250","P_500", "Summary", "Heteros")
+#       
+#       invs<-unique(all_3c$Inversion)
+#       
+#       tmp<-data.frame(matrix(vector(), nrow = length(invs), ncol=cols, dimnames = list(c(1:length(invs)), c("Inversion",colist))), stringsAsFactors = FALSE )
+#       tmp$Inversion<-invs
+#       
+#       # For each inversion and population, make a summary
+#       for (inv in tmp$Inversion) {
+#         
+#         tmp_2<-all_3c[all_3c$Inversion == inv & all_3c$Population == pop,]
+#         
+#         if(pop %in% "ALL"){
+#           imput<-as.character(tmp_2[1,"GLB"])
+#           popmean<-NA
+#         }else if(pop %in% c("EUR", "AFR")){
+#           imput<-as.character(tmp_2[1,pop])
+#           popmean<- mean(as.numeric(tmp_2$Probability_con))
+#         }else{
+#           imput<-NA
+#           popmean<- mean(as.numeric(tmp_2$Probability_con))
+#         }
+#         
+#         # Content of the summary
+#         content<-c(
+#           pop,
+#           imput,
+#           table(tmp_2$Uniformity)["good"]/sum(table(tmp_2$Uniformity)),
+#           popmean,
+#           mean(as.numeric(tmp_2$Probability_100)),
+#           mean(as.numeric(tmp_2$Probability_250)),
+#           mean(as.numeric(tmp_2$Probability_500)) ,
+#           sum(table(tmp_2[tmp_2$Uniformity == "good","Genotype_100"]) > 0),
+#           table(tmp_2[tmp_2$Uniformity == "good","Genotype_100"])["HET"] 
+#         )
+#         
+#         tmp[tmp$Inversion == inv, colist] <- content
+#         
+#       }
+#       
+#       res_list[[pop]]<-tmp
+#       # rm(tmp, tmp_2, content)
+#     }
+#     
+#     inversion_summary<-do.call(rbind, res_list)
+#     inversion_summary<-inversion_summary[order(inversion_summary$Inversion),]
+#     
+#     inversion_summary[is.na(inversion_summary$Heteros),"Heteros"]<-0
+#     
+#     return(inversion_summary)
+#   }
+#   
+#   sheet2<-summarize(all_3c)
+#   write.csv(sheet2,   paste0(args[4],"/summaryByInversion.csv"), row.names = FALSE) 
 
 # OUTPUT GENOTYPES FILE
 # Automatically selected genotypes that can be considered good quality
@@ -236,44 +242,44 @@ lapply(packages,FUN = function(x) {
 # OUTPUT GENOTYPES FILE - Definitive accepted genotypes
 # --------------------------------------------------------------------------- #  
 
-  # Take imputable inversions
-  imputable<-as.character(imputability[!(imputability$GLB %in% c("No_Imputable")|imputability$AFR%in% c("No_Imputable")|imputability$EUR%in% c("No_Imputable")), "Inversion"])
+  # # Take imputable inversions
+  # imputable<-as.character(imputability[!(imputability$GLB %in% c("No_Imputable")|imputability$AFR%in% c("No_Imputable")|imputability$EUR%in% c("No_Imputable")), "Inversion"])
+  # 
+  # # Make it as a function to avoid intermediate variables
+  # makeGenotypes<-function(all_3c, imputable){
+  #   
+  #   # Take interesting inversions
+  #   part<-all_3c[all_3c$Inversion %in% imputable, ]
+  # 
+  #   # Make a case marker
+  #   part$accepted<-NA
+  # 
+  #   # Accepted case 1: good uniformity and good probability
+  #   part[part$Uniformity == "good" & 
+  #          part$Quality == "good" , "accepted" ]<-"yes"
+  #   
+  #   # Accepted case 2: good uniformity and alert probability but good probability in population-specific
+  #   part[is.na(part$accepted) & part$Uniformity == "good" & part$Quality == "alert" & 
+  #          part$Probability_con > 0.8  &  !is.na(part$Probability_con ), "accepted" ]<-"yes"
+  #   
+  #   # Accepted case 3: FOR LESS STRICT FILTERING - all with good uniformity
+  #   part[is.na(part$accepted) & part$Uniformity == "good" , "accepted" ]<-"yes"
+  #   
+  #   # Accepted case 4: FOR LESS STRICT FILTERING - bad uniformity but good probability in population-specific
+  #   part[is.na(part$accepted) & part$Uniformity != "good" & 
+  #          part$Probability_con > 0.8  & !is.na(part$Probability_con), "accepted"]<-"pop"
+  #   
+  #   # Transform to genotypes table
+  #   p1<-part[part$accepted %in% c("yes"),c("Inversion", "Individual", "Genotype_100")]
+  #   p2<-part[part$accepted %in% c("pop"),c("Inversion", "Individual", "Genotype_con")]
+  #   colnames(p1)<-colnames(p2)
+  #   p<-rbind(p1,p2 )
+  #   w<-reshape2::dcast(p, Individual ~ Inversion)
+  #   
+  #   return(w)
+  # }
   
-  # Make it as a function to avoid intermediate variables
-  makeGenotypes<-function(all_3c, imputable){
-    
-    # Take interesting inversions
-    part<-all_3c[all_3c$Inversion %in% imputable, ]
-  
-    # Make a case marker
-    part$accepted<-NA
-  
-    # Accepted case 1: good uniformity and good probability
-    part[part$Uniformity == "good" & 
-           part$Quality == "good" , "accepted" ]<-"yes"
-    
-    # Accepted case 2: good uniformity and alert probability but good probability in population-specific
-    part[is.na(part$accepted) & part$Uniformity == "good" & part$Quality == "alert" & 
-           part$Probability_con > 0.8  &  !is.na(part$Probability_con ), "accepted" ]<-"yes"
-    
-    # Accepted case 3: FOR LESS STRICT FILTERING - all with good uniformity
-    part[is.na(part$accepted) & part$Uniformity == "good" , "accepted" ]<-"yes"
-    
-    # Accepted case 4: FOR LESS STRICT FILTERING - bad uniformity but good probability in population-specific
-    part[is.na(part$accepted) & part$Uniformity != "good" & 
-           part$Probability_con > 0.8  & !is.na(part$Probability_con), "accepted"]<-"pop"
-    
-    # Transform to genotypes table
-    p1<-part[part$accepted %in% c("yes"),c("Inversion", "Individual", "Genotype_100")]
-    p2<-part[part$accepted %in% c("pop"),c("Inversion", "Individual", "Genotype_con")]
-    colnames(p1)<-colnames(p2)
-    p<-rbind(p1,p2 )
-    w<-reshape2::dcast(p, Individual ~ Inversion)
-    
-    return(w)
-  }
-  
-  genotypes<-makeGenotypes(all_3c, imputable)
-  write.table(genotypes,   paste0(args[4],"/new_genotypes.csv"), quote = FALSE, sep = "\t", na = ".", row.names = FALSE, col.names = TRUE) 
+  # genotypes<-makeGenotypes(all_3c, imputable)
+  write.table(all_3c,   paste0(args[4],"/new_genotypes.csv"), quote = FALSE, sep = "\t", na = ".", row.names = FALSE, col.names = TRUE) 
   
   
